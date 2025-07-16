270c193c779ae742b06c1e52491ef8cd6a295ea8
Rehan Ahmad <r280822a@gmail.com>
2025-07-16 19:07:27 +0100

    Add script to set YouTube resolution to 4K on fullscreen

--- YouTube Highest Quality on Fullscreen-1.0.user.js
+++ YouTube Highest Quality on Fullscreen-1.0.user.js
@@ -1,15 +1,41 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         YouTube 4K on Fullscreen
 // @namespace    http://tampermonkey.net/
 // @version      1.0
-// @description  try to take over the world!
-// @author       You
-// @match        http://*/*
+// @description  Set YouTube resolution to 4K (2160p) when entering fullscreen
+// @author       Rehan Ahmad
+// @match        https://www.youtube.com/*
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
-    // Your code here...
+    function setResolutionTo4K() {
+        const player = document.getElementById('movie_player');
+        if (!player) return;
+
+        const qualities = player.getAvailableQualityLevels?.();
+        if (!qualities || !qualities.includes('hd2160')) {
+            console.warn('4K not available. Available qualities:', qualities);
+            return;
+        }
+
+        console.log('Setting to 4K');
+        player.setPlaybackQualityRange?.('hd2160');
+        player.setPlaybackQuality?.('hd2160');
+    }
+
+    document.addEventListener('fullscreenchange', () => {
+        if (document.fullscreenElement) {
+            setTimeout(setResolutionTo4K, 500);
+        }
+    });
+
+    // If already fullscreen on load
+    window.addEventListener('load', () => {
+        if (document.fullscreenElement) {
+            setTimeout(setResolutionTo4K, 1000);
+        }
+    });
 })();
