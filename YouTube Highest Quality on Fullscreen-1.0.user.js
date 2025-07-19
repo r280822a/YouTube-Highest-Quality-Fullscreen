eaf621a5556aa8b0ccdedcbc4ce47c4df51d7b83
Rehan Ahmad <r280822a@gmail.com>
2025-07-19 20:13:16 +0100

    Add toast to show when resolution has changed

--- YouTube Highest Quality on Fullscreen-1.0.user.js
+++ YouTube Highest Quality on Fullscreen-1.0.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube Highest Quality on Fullscreen
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  Set YouTube video to highest available resolution when entering fullscreen (e.g. 4K, 1440p, 1080p, etc.)
 // @author       Rehan Ahmad
 // @match        https://www.youtube.com/*
@@ -11,20 +11,36 @@
 (function () {
     'use strict';
 
+    function showToast(message) {
+        let toast = document.querySelector('tp-yt-paper-toast#customtoast');
+        if (toast) { // Delete if still visible to avoid unnecessary queueing
+            toast.remove();
+        }
+
+        toast = document.createElement('tp-yt-paper-toast');
+        toast.id = 'customtoast'
+        toast.innerText = message;
+        document.body.appendChild(toast);
+
+        toast.show();
+    }
+
     function setHighestResolution() {
         const player = document.getElementById('movie_player');
         if (!player) return;
 
         const qualities = player.getAvailableQualityLevels?.();
+        console.log('All qualities:', qualities);
         if (!qualities || qualities.length === 0) {
             console.warn('No available resolutions found.');
             return;
         }
 
         const highest = qualities[0]; // YouTube lists highest resolution first
-        console.log('Setting resolution to highest available:', highest);
         player.setPlaybackQualityRange?.(highest);
         player.setPlaybackQuality?.(highest);
+        console.log('Resolution set to:', highest);
+        showToast('Resolution set to ' + highest);
     }
 
     document.addEventListener('fullscreenchange', () => {
